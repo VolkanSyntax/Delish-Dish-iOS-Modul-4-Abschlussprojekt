d0098ea469cd3d8a121024129854292c35d3f432
VolkanSyntax <148045998+VolkanSyntax@users.noreply.github.com>
2024-07-02 01:35:47 +0200

    erste commit probieren

---
+++ DelishDish/DelishDish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DelishDish
+//
+//  Created by Volkan Yücel on 02.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DelishDish/DelishDish/DelishDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelishDishApp.swift
+//  DelishDish
+//
+//  Created by Volkan Yücel on 02.07.24.
+//
+
+import SwiftUI
+
+@main
+struct DelishDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
