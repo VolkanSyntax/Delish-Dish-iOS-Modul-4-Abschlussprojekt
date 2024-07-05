e6ce14148512c57c61b8e1353f8c3385f6eff66d
VolkanSyntax <148045998+VolkanSyntax@users.noreply.github.com>
2024-07-05 17:42:16 +0200

    ff

--- DelishDish/DelishDish/Login/AuthentificationView.swift
+++ DelishDish/DelishDish/Login/AuthentificationView.swift
@@ -20,13 +20,41 @@ struct AuthentificationView: View {
             Button("Anmelden"){
                 anmeldenToggle()
             }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(.white)
+            .bold()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            Button("Registrieren"){
+                registrierenToggle()
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(.white)
+            .bold()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
             
         }
+        .padding()
+        
         .sheet(isPresented: $anmelden){
             NavigationView{
                 LoginView()
                     .navigationBarItems(trailing: Button(action: {
                         anmelden.toggle()
+                    }){
+                        Image(systemName: "xmark")
+                    })
+            }
+        }
+        .sheet(isPresented: $registrieren){
+            NavigationView{
+                RegistrierenView()
+                    .navigationBarItems(trailing: Button(action: {
+                        registrieren.toggle()
                     }){
                         Image(systemName: "xmark")
                     })
@@ -43,6 +71,13 @@ struct AuthentificationView: View {
     func anmeldenToggle() {
         anmelden.toggle()
     }
+    
+    
+    func registrierenToggle() {
+        registrieren.toggle()
+    }
+    
+    
 }
 
 
