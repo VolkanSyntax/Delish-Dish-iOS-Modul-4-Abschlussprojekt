06c527215561af142a37dcfcfe0aac7343e65a94
VolkanSyntax <148045998+VolkanSyntax@users.noreply.github.com>
2024-08-18 13:59:18 +0200

    kommentar eklendi.. 18.08.2024

--- DelishDish/DelishDish/ToDoListView/ToDoListView.swift
+++ DelishDish/DelishDish/ToDoListView/ToDoListView.swift
@@ -9,34 +9,59 @@ import SwiftUI
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
+
 struct ToDoListView: View {
     
     @StateObject private var todoListViewModel = ToDoListViewModel()
     
     
-    @State private var showNewToDoItem = false
-    @State private var showEditToDoItem = false
-    @State private var showShareAlert = false
-    @State private var showShareResultAlert = false
-    @State private var shareResultMessage = ""
-    @State private var userEmailToShare = ""
-    @State private var selectedToDoItem: FireToDoItem?
     
+    // Zustand, um die Benutzeroberfläche zum Hinzufügen eines neuen To-Do-Elements zu öffnen und zu schließen.
+    // Yeni bir yapılacak öğesi eklemek için kullanıcının arayüzünü açıp kapatmak için kullanılan durum.
+    @State private var showNewToDoItem = false
+    // Zustand, um die Benutzeroberfläche zum Bearbeiten des ausgewählten To-Do-Elements zu öffnen und zu schließen.
+    // Seçili yapılacak öğeyi düzenlemek için kullanıcının arayüzünü açıp kapatmak için kullanılan durum.
+    @State private var showEditToDoItem = false
+    // Zustand, um das Freigabe-Alarmfenster anzuzeigen.
+    // Paylaşım uyarı penceresini göstermek için kullanılan durum.
+    @State private var showShareAlert = false
+    // Zustand, um das Benachrichtigungsfenster mit dem Freigabeergebnis anzuzeigen.
+    // Paylaşım sonucunu gösteren uyarı penceresini göstermek için kullanılan durum.
+    @State private var showShareResultAlert = false
+    // Speichert das Ergebnis des Freigabevorgangs.
+    // Paylaşım işleminin sonucunu saklar.
+    @State private var shareResultMessage = ""
+    // Speichert die E-Mail des Benutzers, mit dem das To-Do-Element geteilt wird.
+    // Yapılacak öğenin paylaşılacağı kullanıcının e-posta adresini saklar.
+    @State private var userEmailToShare = ""
+    // Speichert das aktuell ausgewählte To-Do-Element.
+    // Şu anda seçili olan yapılacak öğeyi saklar.
+    @State private var selectedToDoItem: FireToDoItem?
     var body: some View {
         NavigationStack {
             VStack {
+                // Liste der To-Do-Elemente, die aus dem ViewModel abgerufen wurden.
+                // ViewModel'den alınan yapılacak öğelerin listesi.
                 List(todoListViewModel.todoItems) { item in
                     HStack {
                         VStack(alignment: .leading) {
+                            // Anzeige des Titels des To-Do-Elements.
+                            // Yapılacak öğenin başlığını gösterir.
                             Text(item.title)
                                 .font(.headline)
                                 .foregroundColor(.red)
+                            
+                            // Anzeige der Beschreibung des To-Do-Elements.
+                            // Yapılacak öğenin açıklamasını gösterir.
                             Text(item.description)
                                 .font(.body)
                                 .lineLimit(2)
                         }
                         Spacer()
+                        
                         if item.isCompleted {
+                            // Zeigt ein Häkchen, wenn das To-Do-Element abgeschlossen ist.
+                            // Yapılacak öğe tamamlandığında bir onay işareti gösterir.
                             Image(systemName: "checkmark.circle")
                                 .foregroundColor(.green)
                                 .font(.system(size: 21))
@@ -44,6 +69,8 @@ struct ToDoListView: View {
                                     todoListViewModel.updateToDoItem(withId: item.id, isCompleted: false)
                                 }
                         } else {
+                            // Zeigt einen leeren Kreis, wenn das To-Do-Element nicht abgeschlossen ist.
+                            // Yapılacak öğe tamamlanmadığında boş bir daire gösterir.
                             Image(systemName: "circle")
                                 .font(.system(size: 21))
                                 .foregroundColor(.gray)
@@ -58,9 +85,14 @@ struct ToDoListView: View {
                         showEditToDoItem = true
                     }
                     .swipeActions {
+                        // Schaltfläche zum Löschen des To-Do-Elements.
+                        // Yapılacak öğeyi silmek için düğme.
                         Button("Löschen", role: .destructive) {
                             todoListViewModel.deleteToDoItem(withId: item.id)
                         }
+                        
+                        // Schaltfläche zum Teilen des To-Do-Elements.
+                        // Yapılacak öğeyi paylaşmak için düğme.
                         Button(action: {
                             showShareAlert = true
                             selectedToDoItem = item
@@ -71,10 +103,14 @@ struct ToDoListView: View {
                     }
                 }
                 .onAppear {
+                    // Lädt die To-Do-Elemente, wenn die Ansicht erscheint.
+                    // Görünüm göründüğünde yapılacak öğeleri yükler.
                     todoListViewModel.fetchToDoItems()
                 }
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
+                        // Schaltfläche zum Hinzufügen eines neuen To-Do-Elements.
+                        // Yeni bir yapılacak öğe eklemek için düğme.
                         Button(action: {
                             showNewToDoItem = true
                         }) {
@@ -83,10 +119,14 @@ struct ToDoListView: View {
                     }
                 }
                 .alert("ToDo Teilen", isPresented: $showShareAlert, actions: {
+                    // Eingabefeld für die E-Mail des Benutzers, mit dem das To-Do-Element geteilt werden soll.
+                    // Yapılacak öğenin paylaşılacağı kullanıcının e-posta adresini girmek için giriş alanı.
                     TextField("Benutzer E-Mail", text: $userEmailToShare)
                         .autocorrectionDisabled(true)
                         .autocapitalization(.none)
                     HStack {
+                        // Schaltfläche zum Bestätigen des Teilens.
+                        // Paylaşımı onaylamak için düğme.
                         Button("Teilen") {
                             if let item = selectedToDoItem {
                                 todoListViewModel.shareToDoItem(withId: item.id, withUserEmail: userEmailToShare) { success in
@@ -97,6 +137,8 @@ struct ToDoListView: View {
                             userEmailToShare = ""
                             showShareAlert = false
                         }
+                        // Schaltfläche zum Abbrechen der Freigabe.
+                        // Paylaşımı iptal etmek için düğme.
                         Button("Abbrechen") {
                             userEmailToShare = ""
                             showShareAlert = false
@@ -105,13 +147,19 @@ struct ToDoListView: View {
                     }
                 })
                 .alert(shareResultMessage, isPresented: $showShareResultAlert) {
+                    // Schaltfläche zum Schließen des Freigabeergebnis-Alerts.
+                    // Paylaşım sonucu uyarısını kapatmak için düğme.
                     Button("OK", role: .cancel) { }
                 }
                 .navigationDestination(isPresented: $showNewToDoItem) {
+                    // Zeigt die Ansicht zum Hinzufügen eines neuen To-Do-Elements an.
+                    // Yeni bir yapılacak öğe ekleme görünümünü gösterir.
                     AddToDoListView(todoListViewModel: todoListViewModel, isPresented: $showNewToDoItem)
                 }
                 .navigationDestination(isPresented: $showEditToDoItem) {
                     if let selectedItem = selectedToDoItem {
+                        // Zeigt die Ansicht zum Bearbeiten des ausgewählten To-Do-Elements an.
+                        // Seçili yapılacak öğeyi düzenleme görünümünü gösterir.
                         EditToDoListView(
                             isPresented: $showEditToDoItem,
                             todoListViewModel: todoListViewModel,
@@ -119,12 +167,16 @@ struct ToDoListView: View {
                         )
                     }
                 }
+                // Setzt den Titel der Navigationsleiste.
+                // Gezinti çubuğu başlığını ayarlar.
                 .navigationTitle("Meine ToDo Liste")
                 .navigationBarTitleDisplayMode(.inline)
             }
         }
         .onChange(of: showShareAlert) { oldValue, newValue in
             if newValue == false {
+                // Löscht die Benutzer-E-Mail, wenn das Freigabe-Alertfenster geschlossen wird.
+                // Paylaşım uyarı penceresi kapatıldığında kullanıcı e-postasını temizler.
                 userEmailToShare = ""
             }
         }
